f3706049bc8a7526112f5599edcd62e0209f1051
Dursun İNEL <dursun.inel@mrevyap.k12.tr>
2020-05-22 01:51:47 +0300

    Güncellendi ve Tamamlandı

--- Calismalarim/calisma3/soru5.js
+++ Calismalarim/calisma3/soru5.js
@@ -1,16 +1,17 @@
-let sinirlama= document.querySelector("#area");   
- let yazi=document.querySelector("#yazi");   
-  console.log(sinirlama) 
-  sinirlama.addEventListener("keyup",KarakterSayisi);
-   function KarakterSayisi() {       
-          var sonsayi = 100 - document.getElementById("mesaj").value.length; 
-                  if (sonsayi >= 0)    
-                       {         
-                               document.getElementById("sayac").innerHTML = sonsayi;     
-                                } 
-                                        else  
-                                               {     
-                                                           document.getElementById("mesaj").value = document.getElementById("mesaj").value.substring(0, 100);
-                       document.getElementById("karakter").innerHTML = 0;    
-                         } 
-                        }
+let text = document.querySelector("#yazi");
+
+text.addEventListener("keyup", KarakterSinir);
+
+    function KarakterSinir() 
+    {
+        var sinir = 100 - document.getElementById("yazi").value.length;
+        if (sinir >= 0) 
+        {
+           document.getElementById("sinir").innerHTML = sinir;
+        }
+        else 
+        { 
+           text.value = text.value.substring(0, 100);
+           document.getElementById("sinir").innerHTML = 0; 
+        } 
+   }
