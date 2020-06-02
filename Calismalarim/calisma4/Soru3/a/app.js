9b7e09f102a2feda706498fe5653f769cb21d9cc
Dursun İNEL <dursun.inel@mrevyap.k12.tr>
2020-06-02 04:25:55 +0300

    Tamamlandı.

---
+++ Calismalarim/calisma4/Soru3/a/app.js
@@ -0,0 +1,11 @@
+let options = {
+  title: "Menu",
+  width: 100,
+  height: 200
+};
+
+// Destructing
+
+const {title:a,width:b,height:c} = options;
+
+console.log(a,b,c);
---
+++ Calismalarim/calisma4/Soru3/b/app.js
@@ -0,0 +1,11 @@
+let options = {
+    title: "Menu",
+    width: 100,
+    height: 200
+  };
+  
+// Destructing
+
+const {title:a} = options;
+
+console.log(a);
