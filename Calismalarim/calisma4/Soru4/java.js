8d4ae43f2d685a885c7c09f804c8761322d93432
Dursun İNEL <dursun.inel@mrevyap.k12.tr>
2020-06-13 11:53:21 +0300

    Tamamlandı

---
+++ Calismalarim/calisma4/Soru4/java.js
@@ -0,0 +1,12 @@
+let veri = {
+    size: {
+    width: 100,
+    height: 200
+    },
+    meyve: ["elma", "armut"],
+    varmi: true
+    };
+
+    const {meyve:array} = veri;
+
+    console.log(array);
